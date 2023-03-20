a4ca016e4e04d9c59bbf07f4992ea3681f69a161
MarlonPasseri <126823766+MarlonPasseri@users.noreply.github.com>
2023-03-20 11:16:51 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeAciveClasses()
+        panel.classList.add('active')
+    })
+
+})
+
+function removeAciveClasses() {
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+    })
+}
